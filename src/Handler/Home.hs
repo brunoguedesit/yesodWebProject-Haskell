11248801f2c85899f137e13e7df8635541c89ddf
Bruno Guedes <brunoguedesit@gmail.com>
2020-11-24 02:21:17 -0300

    Adicionando a opcao de listar os produtos e logar no menu principal

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -12,12 +12,28 @@ import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do 
+    addStylesheet (StaticR css_bootstrap_css)
+    sess <- lookupSession "_EMAIL"
     [whamlet| 
             <h1>
                 SISTEMA DE DOAÇÃO
-
             <ul>
                 <li>
                     <a href=@{ProdutoR}>
                         CADASTRO DE PRODUTO PARA DOAÇÃO
+
+                <li>
+                    <a href=@{ListProdR}>
+                        LISTAR PRODUTOS DISPONÍVEIS PARA DOAÇÃO
+        
+                $maybe email <- sess
+                    <li>
+                        <div>
+                            #{email}
+                            <form method=post action=@{SairR}>
+                                <input type="submit" value="Sair">
+                $nothing
+                    <li>
+                        <a href=@{EntrarR}>
+                            LOGAR
     |]
