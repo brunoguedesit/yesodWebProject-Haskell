a7ab5bff8cb007daf696cd302e0847f57c7eadbe
Bruno Guedes <brunoguedesit@gmail.com>
2020-11-23 23:50:50 -0300

    Criando a tabela, rota e o form do Usuario

---
+++ src/Handler/Usuario.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Usuario where
+
+import Import
+
+formUsuario :: Form (Usuario, Text)
+formUsuario = renderBootstrap $ (,)
+    <$> (Usuario
+        <$> areq textField "Nome: " Nothing
+        <*> areq emailField "E-mail: " Nothing
+        <*> areq passwordField "Senha: " Nothing)
+    <*> areq passwordField "Digite Novamente: " Nothing
+
