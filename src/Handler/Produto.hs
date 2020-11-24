1aaae80bf2ba33dec641545797ddb2a7268395cd
Bruno Guedes <brunoguedesit@gmail.com>
2020-11-23 21:49:33 -0300

    Criando form do cadastro de produto e a pagina de descricao do mesmo

--- src/Handler/Produto.hs
+++ src/Handler/Produto.hs
@@ -3,14 +3,50 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.Produto where
 
 import Import
---import Network.HTTP.Types.Status
-import Database.Persist.Postgresql
+--import Database.Persist.Postgresql
+
+formProduto :: Form Produto
+formProduto = renderDivs $ Produto
+    <$> areq textField (FieldSettings "Nome: " 
+                                      Nothing
+                                      (Just "hs12")
+                                      Nothing
+                                      [("class","myClass")]
+                        ) Nothing
+    <*> areq intField "Quantidade: " Nothing
 
 getProdutoR :: Handler Html
-getProdutoR = undefined
+getProdutoR = do
+    (widget,_) <- generateFormPost formProduto
+    defaultLayout $ do
+        [whamlet|
+            <h1>
+                Cadastre um produto para doação
+
+            <form action=@{ProdutoR} method=post>
+                ^{widget}
+                <input type="submit" value="Cadastrar">
+        |]
 
 postProdutoR :: Handler Html
-postProdutoR = undefined
+postProdutoR = do 
+    ((resp,_),_)  <- runFormPost formProduto
+    case resp of
+        FormSuccess produto -> do
+            pid <- runDB $ insert produto
+            redirect (DescR pid)
+        _ -> redirect HomeR
+
+getDescR :: ProdutoId -> Handler Html
+getDescR pid = do
+    produto <- runDB $ get404 pid
+    defaultLayout [whamlet|
+        <h1>
+            Nome: #{produtoNome produto}
+        <h2>
+            Quantidade: #{produtoQuantidade produto}
+                  |]
